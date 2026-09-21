4641d9b6e4958c6c2174e66f57f0471537fc80e4
Ayush Kharya <ayushkharya04@gmail.com>
2026-09-21 20:16:10 +0530

    updated code

---
+++ models/bookingModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required : true
+  },
+  event :{
+    type : mongoose.Schema.Types.ObjectId,
+    ref :"Event",
+    required : true
+  },
+  numberofSeats :{
+    type : Number,
+    required : true,
+    min :1
+  },
+  totalPrice : {
+    type: Number,
+    required: true,
+    min: 0
+  },
+   status: {
+    type: String,
+    enum: ["confirmed", "cancelled"],
+    default: "confirmed"
+    }
+
+});
+
+module.exports = mongoose.model("Booking",bookingSchema);
--- server.js
+++ server.js
@@ -12,13 +12,15 @@ const categoryRoutes = require("./routes/categoryRoutes")
 const testRoutes = require("./routes/testRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const authRoutes = require("./routes/authRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
 
 
 app.use(express.json());
 app.use("/api/categories",categoryRoutes);
 app.use("/api/test", testRoutes);
 app.use("/api/events", eventRoutes);
-app.use("/api/auth",authRoutes)
+app.use("/api/auth",authRoutes);
+app.use("/api/bookings", bookingRoutes);
 app.get("/",(req,res) =>{
   res.json({
     message : "api is running"
